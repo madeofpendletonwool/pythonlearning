08711de6d6e34e97437f8e0ee1ef9f9ce7eef0f3
Collin Pendleton <blacanwite@gmail.com>
2022-08-10 07:05:27 -0500

    Create magic_trick_starter.py

---
+++ magic_trick_starter.py
@@ -0,0 +1,186 @@
+#   __  __             _      
+#  |  \/  |           (_)     
+#  | \  / | __ _  __ _ _  ___ 
+#  | |\/| |/ _` |/ _` | |/ __|
+#  | |  | | (_| | (_| | | (__ 
+#  |_|  |_|\__,_|\__, |_|\___|
+#                 __/ |       
+#                |___/        
+
+# Create a variable called secret_num and set it to any number you'd like:
+secret=10077
+
+# Increase secret_num by 1
+secret+=1
+
+# Next, take the new number and double it
+secret*=2
+
+# Next, take the new number and add four to it
+secret+=4
+
+# Next, take the new number and divide by two
+secret/=2
+
+# Next, take the new number and subtract your original secret number from it
+secret-=10077
+
+# Print the final result
+print(secret)
+
+
+# To see my magical prediction, scroll down...
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+
+#   ____    
+#  |___ \   
+#    __) |  
+#   |__ <   
+#   ___) |  
+#  |____/   
+          
+# DID YOU SEE 3 PRINTED OUT???
